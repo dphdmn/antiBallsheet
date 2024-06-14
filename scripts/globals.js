c35ed32d9595dad210b4df901959f9f44d8219b2
dphdmn <pwfan1000@gmail.com>
2024-06-14 23:24:59 +0800

    small restruct

--- scripts/globals.js
+++ scripts/globals.js
@@ -22,3 +22,8 @@ const colorPickers = [
     { styleName: '#ball', cssProperty: 'backgroundColor', captionText: 'Target color', initialColor: DEFAULT_COLORS.ballBackground },
     { styleName: '.info', cssProperty: 'color', captionText: 'Info text color', initialColor: DEFAULT_COLORS.title }
 ];
+const settings = [
+    { label: 'Balls to catch: ', id: 'balls-to-catch', valueId: 'balls-to-catch-value', min: 50, max: 500, value: 350, step: 50, labels: [50, 100, 150, 200, 250, 300, 350, 400, 450, 500] },
+    { label: 'Ball size: ', id: 'ball-size', valueId: 'ball-size-value', min: 5, max: 20, value: 10, labels: ['20%', '15%', '10%', '5%'] },
+    { label: 'Cursor size (visual only): ', id: 'cursor-size', valueId: 'cursor-size-value', min: 0, max: 64, value: 8, step: 2, labels: [64, 56, 48, 40, 32, 24, 16, 8, 0] }
+];
